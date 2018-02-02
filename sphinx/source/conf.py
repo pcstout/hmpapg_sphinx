21c491df91b99574781bdd614cccb5f1412f1e05
Patrick <pstout@prevagroup.com>
2018-02-02 07:08:54 -0700

    Changed paths and makefile

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -88,10 +88,6 @@ todo_include_todos = True
 #
 html_theme = 'sphinx_rtd_theme'
 
-html_theme_options = {
-    
-}
-
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
