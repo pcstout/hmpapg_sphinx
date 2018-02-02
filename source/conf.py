f7a28ec48f61f459580f0f7cd9b628751665adbf
Patrick <pstout@prevagroup.com>
2018-02-02 12:02:55 -0700

    Updates

--- source/conf.py
+++ source/conf.py
@@ -93,7 +93,7 @@ html_theme = 'sphinx_rtd_theme'
 # documentation.
 #
 html_theme_options = {
-    'collapse_navigation': True
+    'collapse_navigation': False
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
